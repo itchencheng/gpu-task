85aa74b55b759fc09330e88ca0ef0c278fde8201
itchencheng <itchencheng@gmail.com>
2018-04-14 16:15:53 +0800

    ok

--- testOpencl/setup_opencl.cpp
+++ testOpencl/setup_opencl.cpp
@@ -1,6 +1,61 @@
 
-cl_int SetupOpenCL(void *pCtxHandle)
-{
-	/* get platform info */
+#include"setup_opencl.h"
 
+using namespace std;
+
+cl_int CreateContext()
+{
+	cl_int errNum;
+	
+	/* get platform info */
+	cl_uint numPlatforms;
+	cl_platform_id firstPlatformId;
+	errNum = clGetPlatformIDs(1, &firstPlatformId, &numPlatforms);
+	if(CL_SUCCESS != errNum)
+	{
+		cerr << "Failed to find andy OpenCL platforms." <<endl;
+		return NULL;
+	}
+
+	/* create Context */
+	cl_context context = NULL; /* cl_context 使用NULL来初始化 ? */
+	/* GPU */
+	cl_context_properties contextProperties[] = 
+	{
+		CL_CONTEXT_PLATFORM, (cl_context_properties)firstPlatformId,
+		0
+	}
+	context = clCreateContextFromType( 
+		contextProperties,
+		CL_DEVICE_TYPE_GPU,
+		NULL,
+		NULL,
+		&errNum
+		);
+	if(CL_SUCCESS != errNum)
+	{
+		cout << "could not create GPU context, trying CPU..."<<endl;
+		/* CPU */
+		context = clCreateContextFromType(
+			contextProperties,
+			CL_DEVICE_TYPE_GPU,
+			NULL,
+			NULL,
+			&errNUm
+			);
+		if(CL_SUCCESS != errNum)
+		{
+			cerr << "Failed to create CPU context, too."<<end;
+			return NULL;
+		}
+	}
+}
+
+
+/* pick a device, create a command queue */
+cl_command_queue CreateCommandQueue(
+	cl_context context,
+	cl_device_id *device
+)
+{
 }
--- testOpencl/setup_opencl.h
+++ testOpencl/setup_opencl.h
@@ -1,5 +1,27 @@
+#ifndef __SETUP_OPENCL
+#define __SETUP_OPENCL
+
+
+/*
+ * 0: no C++ OOP version yet 
+ */
+#define CPP_STYLE 0
+
+
+
+#if CPP_STYLE
+/* C++ style interface */
+
+#else
 
 /* C style interface */
 struct CtxHandle{
 	
 };
+
+
+#endif //CPP_STYLE
+
+
+
+#endif //__SETUP_OPENCL
